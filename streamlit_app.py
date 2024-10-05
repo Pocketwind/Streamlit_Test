c4ded91e5a4df243f702f7732ebada7e97ff4431
집에가고싶다 <cmshzl0727@gmail.com>
2024-10-05 17:25:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-data=px.data.gapminder()
+data=px.data.gapminder().query("year==2007")
 st.dataframe(data)
-st.bar_chart(data)
+fig=px.treemap(data,path=[px.Constant("World"),"continent","country"],values="pop",color="lifeExp")
