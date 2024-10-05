97c1f8fa7b26ed6afd3e558a51422bacda461799
집에가고싶다 <cmshzl0727@gmail.com>
2024-10-05 17:41:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ st.dataframe(data)
 data1=px.data.tips()
 fig1=px.treemap(data1,path=[px.Constant("All"),"day","time","sex"],values="tip")
 st.plotly_chart(fig1)
+
+data2=px.data.tips()
+fig2=px.pie(data2,values="tip",names="sex",title="성별간 팁 차이")
+st.plotly_chart(fig2)
