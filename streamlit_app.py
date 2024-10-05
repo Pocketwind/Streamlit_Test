a4bf5a08608117c7891ae3757b6b867a6da86836
집에가고싶다 <cmshzl0727@gmail.com>
2024-10-05 17:31:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ st.dataframe(data)
 fig1=px.treemap(data,path=[px.Constant("World"),"continent","country"],values="pop",color="lifeExp")
 st.plotly_chart(fig1)
 
-data=px.data.gapminder().quary("iso_alpha=='JPN' and iso_alpha=='KOR')
+data=px.data.gapminder().quary("iso_alpha=='JPN' and iso_alpha=='KOR'")
 fig2=px.line(data,x="year",y="pop",color="country")
 st.plotly_chart(fig2)
